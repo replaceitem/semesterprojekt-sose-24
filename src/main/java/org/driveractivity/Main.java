f8ae751aa8d18dc2f941f8661be0b0a5dcc74636
wehnicla <niclas.weh@hs-furtwangen.de>
2024-05-07 15:25:56 +0200

    Added everything to Main.java

--- src/main/java/org/driveractivity/Main.java
+++ src/main/java/org/driveractivity/Main.java
@@ -1,7 +1,22 @@
 package org.driveractivity;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class Main extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/org/driveractivity/gui/main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
