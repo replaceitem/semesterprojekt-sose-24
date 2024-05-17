68e6cffa9e51b96ac1b2f610a5752be6e97866a7
replaceitem <40722305+replaceitem@users.noreply.github.com>
2024-05-17 18:32:45 +0200

    Reload by fully re-creating all ActivityBlocks

--- src/main/java/org/driveractivity/gui/ActivityDisplay.java
+++ src/main/java/org/driveractivity/gui/ActivityDisplay.java
@@ -3,10 +3,12 @@ package org.driveractivity.gui;
 import org.driveractivity.entity.Activity;
 import org.driveractivity.service.DriverInterface;
 
+import java.util.List;
+
 public interface ActivityDisplay {
     void load(DriverInterface driverData);
 
-    void reload();
+    void reload(List<Activity> newActivities);
 
     void addBack(Activity activity);
 
