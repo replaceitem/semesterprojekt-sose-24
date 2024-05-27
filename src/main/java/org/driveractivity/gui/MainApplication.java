799d5b462856480a3738839f18f6be103634714d
replaceitem <40722305+replaceitem@users.noreply.github.com>
2024-05-27 21:21:27 +0200

    Seperate Main class from main Application class for jlink

---
+++ src/main/java/org/driveractivity/gui/MainApplication.java
@@ -0,0 +1,27 @@
+package org.driveractivity.gui;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApplication extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+        MainController controller = fxmlLoader.getController();
+
+        controller.setStage(stage);
+
+        stage.setTitle("Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
