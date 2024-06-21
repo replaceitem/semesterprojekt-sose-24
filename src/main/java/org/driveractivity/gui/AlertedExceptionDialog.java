f9b14b7c1b22c83193151b7b8fdf09a7ae0842e8
replaceitem <40722305+replaceitem@users.noreply.github.com>
2024-06-21 16:12:47 +0200

    Added expandable exception stack trace to exception dialog

--- src/main/java/org/driveractivity/gui/AlertedExceptionDialog.java
+++ src/main/java/org/driveractivity/gui/AlertedExceptionDialog.java
@@ -1,19 +1,45 @@
 package org.driveractivity.gui;
 
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import org.driveractivity.exception.AlertedException;
+
+import java.io.*;
 
 public class AlertedExceptionDialog {
     public static void show(AlertedException exception) {
-        exception.printStackTrace(System.err);
-        showSilently(exception);
-    }
-    
-    public static void showSilently(AlertedException exception) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(exception.getTitle());
         alert.setHeaderText(exception.getTitle());
         alert.setContentText(exception.getMessage());
-        alert.show();
+        
+        if(exception.getCause() != null) {
+            exception.printStackTrace(System.err);
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            exception.printStackTrace(pw);
+            String exceptionText = sw.toString();
+
+            Label label = new Label("The exception stacktrace was:");
+
+            TextArea textArea = new TextArea(exceptionText);
+            textArea.setEditable(false);
+            textArea.setWrapText(true);
+            textArea.setStyle("-fx-font-family: 'Monospaced'");
+
+            textArea.setMaxWidth(Double.MAX_VALUE);
+            textArea.setMaxHeight(Double.MAX_VALUE);
+            GridPane.setVgrow(textArea, Priority.ALWAYS);
+            GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+            GridPane expContent = new GridPane();
+            expContent.setMaxWidth(Double.MAX_VALUE);
+            expContent.add(label, 0, 0);
+            expContent.add(textArea, 0, 1);
+
+            alert.getDialogPane().setExpandableContent(expContent);
+        }
+        
+        alert.showAndWait();
     }
 }
